9c1d1b24191fe23b67e2d599a36e76aeaf84aa6b
GabeGregorioTiago <gabrielgregoriopereiratiago@gmail.com>
2022-06-24 16:20:09 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+ // Crie um array que receba 5 itens e exiba no console.
+   let names = ['Gabriel', 'Anna','Carol','Paula','Henrique']
+   console.log(names)
+// Utilize um método para adicionar um nome ao inicio do array.
+   names.unshift('Olivia')
+   console.log(names)
+
+// Utilize um método para remover o último nome do array.
+   names.pop()
+   console.log(names)  
+ 
+// Utilize um método para adicionar dois nomes ao fim do array.
+   names.push('Dulce','Andrea')
+   console.log(names)
+
+// Utilize um método para remover o primeiro nome do array.
+   names.shift()
+   console.log(names)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+
+ let numbers = [7,5,6,3,8,9,2,1,4]
+ console.log(numbers)
+
+ numbers.sort()
+ console.log(numbers)
